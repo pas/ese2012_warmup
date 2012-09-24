5b57dd2d29b736b21b07e13c08a06843b87f2de0
Pas <pascal@offenes-lernen.ch>
2012-09-24 15:53:14 +0200

    Wrote some tests for TradeItem
    modified:   test/trade_item_test.rb

--- test/trade_item_test.rb
+++ test/trade_item_test.rb
@@ -1,3 +1,24 @@
-class TradeItemTest
-  # To change this template use File | Settings | File Templates.
+require 'test/unit'
+require '../app/Trade_Market/trade_item'
+
+class TradeItemTest < Test::Unit::TestCase
+
+  def test_should_create_item
+    item = Trade_Market::TradeItem.create('sand', 100, nil)
+    assert(item != nil, 'Item should be created')
+  end
+
+  def test_should_have_price
+    item = Trade_Market::TradeItem.create('sand', 100, nil)
+    assert(item.price == 100, 'Price be 100 credits')
+  end
+
+  def test_should_have_name
+    item = Trade_Market::TradeItem.create('john', 100, nil)
+    assert(item.name == 'sand', 'Item should be called \'sand\'')
+  end
+
+  def test_should_have_owner
+    #Not yet implemented because User doesn't exist (yet).
+  end
 end
